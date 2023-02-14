d79ac632aa2e5c863f2430f6da697267cb2b584f
microzys <114230779+microzys@users.noreply.github.com>
2023-02-14 12:51:27 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f33b42475e5befec0e6d955f6d806f33",
-"index.html": "80c596086abc29e53b43e8baaad57089",
-"/": "80c596086abc29e53b43e8baaad57089",
-"main.dart.js": "7527847ac7ff07ed3f573c718bda54e2",
+"index.html": "6e364e37c22219730c4efb94b5506ec5",
+"/": "6e364e37c22219730c4efb94b5506ec5",
+"main.dart.js": "4d3fe6ea9cb30de2d93176078bec6623",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
